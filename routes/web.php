5bdb547f66123a36194eb1c8745d028ce2a9f87d
rc2pc2 <petriccarcc@gmail.com>
2023-08-23 11:56:18 +0200

    added both softDelete and recycle bin for post resource

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Auth::routes();
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', [ AdminDashboardController::class , 'home'])->name('home');
+    Route::get('/posts/deleted', [PostController::class, 'deletedIndex'] )->name('posts.deleted');
+    Route::delete('/posts/deleted/{post}', [PostController::class, 'restore'] )->name('posts.restore');
     Route::resource('/posts', PostController::class);
 });
 
