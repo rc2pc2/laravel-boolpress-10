bf848fabbeaf4ee2d5bca379d8f94571591b4346
rc2pc2 <petriccarcc@gmail.com>
2023-08-24 10:13:55 +0200

    refactoring PostController name on routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,10 @@ Auth::routes();
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', [ AdminDashboardController::class , 'home'])->name('home');
-    Route::get('/posts/deleted', [PostController::class, 'deletedIndex'] )->name('posts.deleted');
-    Route::post('/posts/deleted/{post}', [PostController::class, 'restore'] )->name('posts.restore');
-    Route::delete('/posts/deleted/{post}', [PostController::class, 'obliterate'] )->name('posts.obliterate');
-    Route::resource('/posts', PostController::class);
+    Route::get('/posts/deleted', [AdminPostController::class, 'deletedIndex'] )->name('posts.deleted');
+    Route::post('/posts/deleted/{post}', [AdminPostController::class, 'restore'] )->name('posts.restore');
+    Route::delete('/posts/deleted/{post}', [AdminPostController::class, 'obliterate'] )->name('posts.obliterate');
+    Route::resource('/posts', AdminPostController::class);
 });
 
 Route::name('guest.')->group(function () {
