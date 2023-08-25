1653d165263a5bf2cd56c5ec8aed7f81499923f2
rc2pc2 <petriccarcc@gmail.com>
2023-08-25 12:16:31 +0200

    added one-to-one relationship between  UserDetails and User

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,6 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <div class="card-header">Ciao <strong>{{ Auth::user()->name }}</strong>! Here is your profile: </div>
+
+                <div class="card-body">
+                    <p>
+                        Email: {{ Auth::user()->email }}
+                    </p>
+                    <p>
+                        Address: {{ Auth::user()->userDetail->address }}
+                    </p>
+                    <p>
+                        Office number: {{ Auth::user()->userDetail->office_number }}
+                    </p>
+                    <p>
+                        Date of birth: {{ Auth::user()->userDetail->birth_date }}
+                    </p>
+                    <p>
+                        Signature: {{ Auth::user()->userDetail->signature }}
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
